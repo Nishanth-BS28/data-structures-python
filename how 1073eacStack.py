1041287b448935952f1690661b31b6d2a55b472f
Nishanth BS <aasare2015.nishukeerthi@gmail.com>
2026-06-30 15:53:30 +0530

    Restore stack, queue, bst files

---
+++ how 1073eacStack.py
@@ -0,0 +1,31 @@
+[33mcommit dc854a12bb462b2da5508248b1e9a1bc9c9b8ef0[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Nishanth BS <aasare2015.nishukeerthi@gmail.com>
+Date:   Tue Jun 30 15:39:24 2026 +0530
+
+    Delete Stack.py
+
+ delete mode 100644 Stack.py
+
+[33mcommit f47481dd2c56d093958a06f05012f58ecaff8d04[m
+Author: Nishanth BS <aasare2015.nishukeerthi@gmail.com>
+Date:   Tue Jun 30 15:39:00 2026 +0530
+
+    Delete Queue.py
+
+ delete mode 100644 Queue.py
+
+[33mcommit 67a9f23fe8672bc8db29a133220f05edd7ee0ce9[m
+Author: Nishanth BS <aasare2015.nishukeerthi@gmail.com>
+Date:   Tue Jun 30 15:38:39 2026 +0530
+
+    Delete BST.py
+
+ delete mode 100644 BST.py
+
+[33mcommit 3482427dd85f30cfa9beb6ac2587d34343b6d5d3[m
+Author: Nishanth BS <aasare2015.nishukeerthi@gmail.com>
+Date:   Tue Jun 30 15:38:13 2026 +0530
+
+    Delete Linked list.py
+
+ delete mode 100644 Linked list.py
